db7d0e991dcaacd62d1a18bbebc2ea506eeb11a9
Peter Irlam <irlampeter@gmail.com>
2020-08-25 15:58:11 +0100

    Add short story object

---
+++ shortStory.js
@@ -0,0 +1,19 @@
+let shortStories = {
+	1: {
+		title: 'Skeleton Crew',
+		author: 'Stephen King',
+		favorites: [ 'The Mist', "Mrs Todd's Shortcut", 'The Jaunt', 'Word Processor of the Gods' ]
+	},
+	2: {
+		title: 'Night Shift',
+		author: 'Stephen King',
+		favorites: [ 'The Mangler', 'Gray Matter', 'The Ledge', 'The Lawnmower Man', 'Quitters, Inc.' ]
+	},
+	3: {
+		title: 'The Preserving Machine',
+		author: 'Phillip K Dick'
+	},
+	4: {
+		title: 'I, Robot'
+	}
+};
